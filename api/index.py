ced25a8ce771f68a8056ea93fcb584321ddfe424
XADE <notadevel@gmail.com>
2023-12-12 22:48:00 +0530

    removed repetitions

--- api/index.py
+++ api/index.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, send_file
 import openpyxl
 
 app = Flask(__name__)
+EXCEL_FILE_PATH = '/tmp/students.xlsx'
 
 def get_workbook(excel_file_path):
     try:
@@ -18,48 +19,50 @@ def get_headers():
 def append_headers(worksheet):
     worksheet.append(get_headers())
 
-def append_student_data(worksheet, student_data):
-    headers = get_headers()
-    worksheet.append([student_data.get(field, '') for field in headers])
+def append_data(worksheet, data):
+    worksheet.append(data)
 
 def save_workbook(excel_file_path, workbook):
     workbook.save(excel_file_path)
-    
+
 def save_student_to_excel(student_data):
-    excel_file_path = '/tmp/students.xlsx'
-    workbook = get_workbook(excel_file_path)
+    workbook = get_workbook(EXCEL_FILE_PATH)
     worksheet = get_worksheet(workbook)
 
     if not worksheet.dimensions:
         append_headers(worksheet)
 
-    append_student_data(worksheet, student_data)
-    save_workbook(excel_file_path, workbook)
-    
+    append_data(worksheet, [student_data.get(field, '') for field in get_headers()])
+    save_workbook(EXCEL_FILE_PATH, workbook)
+
 def read_students_from_excel():
-    excel_file_path = '/tmp/students.xlsx'
-    workbook = get_workbook(excel_file_path)
+    workbook = get_workbook(EXCEL_FILE_PATH)
     worksheet = get_worksheet(workbook)
 
-    # Check if the worksheet has any data
-    if worksheet.dimensions:
-        headers = get_headers()
-        students_data = [dict(zip(headers, row)) for row in worksheet.iter_rows(min_row=1, values_only=True)]
-        return students_data
+    if not worksheet.dimensions:
+        return []
 
-    return []
+    headers = get_headers()
+    students_data = []
 
-# def read_students_from_excel():
-#     excel_file_path = '/tmp/students.xlsx'
-#     workbook = get_workbook(excel_file_path)
-#     worksheet = get_worksheet(workbook)
+    for row in worksheet.iter_rows(min_row=2, values_only=True):
+        student_dict = {}
+        for field, value in zip(headers, row):
+            student_dict[field] = value
+        students_data.append(student_dict)
 
-#     if not worksheet.dimensions:
-#         return []
+    return students_data
 
-#     headers = get_headers()
-#     students_data = [dict(zip(headers, row)) for row in worksheet.iter_rows(min_row=2, values_only=True)]
-#     return students_data
+def read_students_from_excel():
+    workbook = get_workbook(EXCEL_FILE_PATH)
+    worksheet = get_worksheet(workbook)
+
+    if not worksheet.dimensions:
+        return []
+
+    headers = get_headers()
+    students_data = [dict(zip(headers, row)) for row in worksheet.iter_rows(min_row=1, values_only=True)]
+    return students_data
 
 @app.route('/')
 def index():
@@ -81,9 +84,8 @@ def render_elements():
 
 @app.route('/clear_data')
 def clear_data():
-    excel_file_path = '/tmp/students.xlsx'
-    workbook = openpyxl.Workbook()    
-    save_workbook(excel_file_path, workbook)
+    workbook = openpyxl.Workbook()
+    save_workbook(EXCEL_FILE_PATH, workbook)
     return render_elements()
 
 @app.route('/generate_excel', methods=['POST'])
@@ -91,7 +93,7 @@ def generate_excel():
     if request.method == 'POST':
         input_data = request.form.to_dict()
         save_student_to_excel(input_data)
-    return send_file('/tmp/students.xlsx', as_attachment=True)
+    return send_file(EXCEL_FILE_PATH, as_attachment=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
