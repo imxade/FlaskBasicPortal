400b116cbb29a203ce1e2e2c5e3dee5c3bf87011
XADE <notadevel@gmail.com>
2023-12-20 01:10:35 +0530

    offline chaneges

---
+++ api/templates/.offCache.js
@@ -0,0 +1,19 @@
+const serviceWorkerScope = '/';
+self.addEventListener('install', function(event) {
+ event.waitUntil(
+   caches.open('offCache').then(function(cache) {
+     return cache.addAll([
+       '/',
+       '/index.html',
+     ]);
+   })
+ );
+});
+
+self.addEventListener('fetch', function(event) {
+ event.respondWith(
+   caches.match(event.request).then(function(response) {
+     return response || fetch(event.request);
+   })
+ );
+});
